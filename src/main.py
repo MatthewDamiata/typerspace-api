a9aa1ca8a312633b6f22c0976cfa2ad9782e217c
MatthewDamiata <mdamiat1@binghamton.edu>
2022-11-19 16:25:22 -0500

    feat: add tests workflow

--- src/main.py
+++ src/main.py
@@ -6,4 +6,9 @@ transcriber = Transcriber()
 
 @app.get("/video/{video_id}")
 def read_transcript(video_id: str):
+    """
+    Returns the transcript of a video in JSON format
+    :param video_id: the id of the video
+    :return: the transcript of the video
+    """
     return transcriber.get_video_json(video_id)
